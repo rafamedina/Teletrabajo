883c8d32cf4bd35fa5a6cf93c277dec9e801c41a
Rafael Medina <rafael.medina.ayuso@gmail.com>
2026-03-16 09:44:30 +0100

    Inicio de Modulo teletrabajo

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Teletrabajo',
+    'version': '1.0.0',
+    'summary': """ Teletrabajo Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', 'web'],
+    'data': [
+        
+    ],
+    'assets': {
+              'web.assets_backend': [
+                  'Teletrabajo/static/src/**/*'
+              ],
+          },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
